80e8a4b251d7fb05673c3bbc2795b0d4cd90b732
Evan Brody <brody_e@outlook.com>
2025-10-01 18:14:31 -0400

    add bias testing

--- inf_duv/inf_duv.py
+++ inf_duv/inf_duv.py
@@ -1,12 +1,19 @@
 # @file     inf_duv.py
 # @author   Evan Brody
-# @brief    Testbed for infinite coin flipping
+# @brief    Testbed for infinite coin flipping sequences
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class InfUVP:
-    def __init__(self):
+    def __init__(self, p_high=None, p_low=None):
         self.generate_coins()
+
+        if p_high is not None:
+            self.p_high = p_high
+        
+        if p_low is not None:
+            self.p_low = p_low
 
     # Guarantees that coins are on both sides of 1/2
     # Other case isn't interesting
@@ -39,8 +46,35 @@ class InfUVP:
 
                 bias_ratio *= self.p_low
                 bias_ratio /= 1 - self.p_low
+        
+        return coin_sequence, bias_sequence
             
+def plot_sequences(n, p_high=None, p_low=None):
+    iuvp = InfUVP(p_high, p_low)
+    coin_sequence, bias_sequence = iuvp.generate_sequence(n)
 
-            
+    fig, ax = plt.subplots()
+    xticks = list(range(n))
 
+    ax.plot(bias_sequence, marker='o', linestyle='None', color='black')
+    ax.set_xticks(xticks)
 
+    # color in the biases
+    bar_height = max(bias_sequence) + 5
+    for j, bias in zip(xticks, bias_sequence):
+        color = 'gray'
+        if bias > 1:
+            color = 'red'
+        elif bias < 1:
+            color = 'blue'
+        
+        ax.bar(j, bar_height, width=1, color=color, alpha=0.3, align='center')
+
+    ax.set_title(f'H = {iuvp.p_high}, T = {iuvp.p_low}')
+    ax.set_xlabel('Coin #')
+    ax.set_ylabel('Bias')
+
+    plt.show()
+
+if __name__ == '__main__':
+    plot_sequences(30, 3/4, 1/8)
